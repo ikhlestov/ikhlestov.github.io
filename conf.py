41e6ba8c0ba04a5e2ebd356a634778def2fb37ac
Khlyestov Illarion <khlyestovillarion@gmail.com>
2016-10-03 03:20:29 +0300

    Nikola auto commit.
    Nikola version: 7.7.9

--- conf.py
+++ conf.py
@@ -843,7 +843,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "ikhlestov-blog.disqus.com"
+COMMENT_SYSTEM_ID = "ikhlestov-blog"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
