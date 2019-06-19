00205b0cea0ac5375c3c46e5e2d1254cd4dcd84c
Illarion Khlestov <khlyestovillarion@gmail.com>
2019-06-19 15:18:47 +0300

    Nikola auto commit.
    Nikola version: 8.0.1

--- conf.py
+++ conf.py
@@ -691,10 +691,11 @@ CODE_COLOR_SCHEME = 'monokai'
 # FAVICONS contains (name, file, size) tuples.
 # Used to create favicon link like this:
 # <link rel="name" href="file" sizes="size"/>
-# FAVICONS = (
-#     ("icon", "/favicon.ico", "16x16"),
-#     ("icon", "/icon_128x128.png", "128x128"),
-# )
+FAVICONS = (
+    ("icon", "/favicon.ico", "16x16"),
+    # ("icon", "/icon_128x128.png", "128x128"),
+    ("icon", "/favicon.png", "128x128"),
+)
 
 # Show teasers (instead of full posts) in indexes? Defaults to False.
 INDEX_TEASERS = True
